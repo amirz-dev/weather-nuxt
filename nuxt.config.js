2ad45dba66e088723946b96495d48aaa3102a1df
amirhossein <amirhossein_zebardast@yahoo.com>
2022-02-08 22:33:59 +0330

    almost done

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~plugins/components.js',
+    '~plugins/date-filter.js',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
