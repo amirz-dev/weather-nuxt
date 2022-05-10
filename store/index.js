6f627f18d070189e5d931d8de56bd750e391adb8
amirhossein <amirhossein_zebardast@yahoo.com>
2022-05-10 13:32:38 +0430

    some code changes

--- store/index.js
+++ store/index.js
@@ -156,86 +156,78 @@ export const actions = {
       locIndex: "first",
     });
   },
-  /*
-  document say key tokens is non expire token 
-  .post("/authorize/token?expire_hours=168", {
-        user: "amirhossein_zebardast",
-        password: "D4PN9m7ZgG3j",
-      })
-      .then(({ data }) => data);
-   */
 
   async getCityInfo(context, payload) {
-    await this.$axios
-      .get(`location/search/${payload.city}`, {
-        headers: {
-          Authorization: context.state.accessToken,
-        },
-      })
-      .then(({ data }) => {
-        const res = data.locations[0];
+    const result = await this.$axios.get(`location/search/${payload.city}`, {
+      headers: {
+        Authorization: context.state.accessToken,
+      },
+    });
 
-        context.dispatch("getDailyWeather", {
-          loc: payload.loc,
-          data: {
-            id: res.id,
-            name: res.name,
-            country: res.country,
-          },
-        });
-      });
+    if (result.status !== 200) return;
+
+    const data = result.data.locations[0];
+
+    context.dispatch("getDailyWeather", {
+      loc: payload.loc,
+      data: {
+        id: data.id,
+        name: data.name,
+        country: data.country,
+      },
+    });
   },
 
   async getDailyWeather(context, payload) {
-    await this.$axios
-      .get(
-        `forecast/daily/${payload.data.id}?windunit=KMH&dataset=full&periods=7`,
-        {
-          headers: {
-            Authorization: context.state.accessToken,
-          },
-        }
-      )
-      .then(({ data }) => {
-        const res = data.forecast;
+    const result = await this.$axios.get(
+      `forecast/daily/${payload.data.id}?windunit=KMH&dataset=full&periods=7`,
+      {
+        headers: {
+          Authorization: context.state.accessToken,
+        },
+      }
+    );
 
-        const weather = [];
+    if (result.status !== 200) return;
 
-        res.forEach((item) => {
-          weather.push({
-            date: item.date,
-            temp: item.maxTemp,
-            wind: item.maxWindSpeed,
-            humidity: item.minRelHumidity,
-            sunrise: item.sunrise,
-            sunset: item.sunset,
-            icon: item.symbol,
-            description: item.symbolPhrase,
-          });
-        });
+    const data = result.data.forecast;
 
-        // set data in location info
-        context.commit("setLocInfo", {
-          loc: payload.loc,
-          data: {
-            ...payload.data,
-            weathers: weather,
-            locIndex: payload.loc,
-          },
-        });
+    const weather = [];
 
-        // set data in active day
-        context.commit("setActiveDay", {
-          ...weather[0],
-          id: 0,
-        });
-
-        // set data in current location
-        context.commit("set_CurrentLoc_and_FilterLocations", {
-          ...payload.data,
-          weathers: weather,
-          locIndex: payload.loc,
-        });
+    data.forEach((item) => {
+      weather.push({
+        date: item.date,
+        temp: item.maxTemp,
+        wind: item.maxWindSpeed,
+        humidity: item.minRelHumidity,
+        sunrise: item.sunrise,
+        sunset: item.sunset,
+        icon: item.symbol,
+        description: item.symbolPhrase,
       });
+    });
+
+    // set data in location info
+    context.commit("setLocInfo", {
+      loc: payload.loc,
+      data: {
+        ...payload.data,
+        weathers: weather,
+        locIndex: payload.loc,
+      },
+    });
+
+    // set data in active day
+    context.commit("setActiveDay", {
+      ...weather[0],
+      id: 0,
+    });
+
+    // set data in current location
+    context.commit("set_CurrentLoc_and_FilterLocations", {
+      ...payload.data,
+      weathers: weather,
+      locIndex: payload.loc,
+    });
   },
 };
